b0d4725d85da76f3382d0ccc56d77ec6252c405a
teemoll <teemo_xh@163.com>
2021-09-28 21:49:32 +0800

    新建项目

---
+++ src/main/java/com/teemoll/aopscaffold/AopScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.teemoll.aopscaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopScaffoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teemoll/aopscaffold/AopScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teemoll.aopscaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopScaffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
